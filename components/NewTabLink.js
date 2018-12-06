9389ea40d853461d2ea87e6f957ac73ae76c6f31
bedbugauthority <dcato98+bbapp@gmail.com>
2018-12-06 13:34:20 -0500

    Make NewTabLink.js a class and add OpenInNew icon

--- components/NewTabLink.js
+++ components/NewTabLink.js
@@ -1,14 +1,29 @@
-const NewTabLink = ({ children, href }) => {
-  const handleClick = e => {
+import { withStyles } from "@material-ui/core/styles";
+import OpenInNew from "@material-ui/icons/OpenInNew";
+
+const styles = theme => ({
+  icon: { width: theme.spacing.unit * 1.5, height: theme.spacing.unit * 1.5 }
+});
+
+class NewTabLink extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleClick = e => {
     e.preventDefault();
-    window.open(href, "_blank");
+    window.open(this.props.href, "_blank");
   };
 
-  return (
-    <a href={href} onClick={handleClick}>
-      {children}
-    </a>
-  );
-};
+  render() {
+    const { children, href, classes } = this.props;
+    return (
+      <a href={href} onClick={this.handleClick}>
+        {children}
+        <OpenInNew className={classes.icon} />
+      </a>
+    );
+  }
+}
 
-export default NewTabLink;
+export default withStyles(styles)(NewTabLink);
